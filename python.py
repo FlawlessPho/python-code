1202cf058c196ddacb5d35987711b46bbdc2a8cb
Phoenix <104746210+FlawlessPho@users.noreply.github.com>
2022-10-20 10:20:19 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,18 @@
+def function_1(x,y):
+# command can be turned into a function as shown reducing num of lines
+#    x = 5
+#    y = 8
+    z = x + y
+    print(z)
+function_1(5,8)
+
+def function_2(x,y):
+#    x = 7
+#    y = 43
+    z = x + y
+    print(z)
+function_2(7,43)
+
+def function_3(function_1,function_2):
+    a = function_1 * function_2
+    print(a)
